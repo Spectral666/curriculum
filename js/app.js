38a917ab857d43c949a6f1ed6df407cda713dd12
Adrian Gomez <adriangcoding@gmail.com>
2021-05-10 12:15:02 +0200

    Añado el idioma español y empiezo el responsive

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 window.onload = function(){
     $('#carga').fadeOut();
 }
+
+function language() {
+    var idioma = document.getElementById('language').value;
+    if (idioma == 'EN') {
+        window.location.href = '../index.html';
+    } else {
+        window.location.href = './es/index.html';
+    }
+}
